dddd09db010ee1842f96cd080eed62ae4549b1c8
PRAJESH BANERJEE <152708730+Prajesh1218@users.noreply.github.com>
2024-04-23 17:01:38 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 545854;
+let accountEmail = "abc@google.com";
+var accountPassword = 12345;
+accountCity = "kolkata";
+let accountState;
+
+// accountId = 548545;
+accountEmail = "abcd@google.com";
+accountPassword = "012345";
+accountCity = "NewDelhi";
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
